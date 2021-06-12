2211e12e5d218bd47bc591ebb8248b00a02eff90
gopakumar.nair <gopakumar.nair@ilearningengines.com>
2021-06-13 00:39:37 +0530

    Fav icon and data text change

--- src/components/Infosection/Data.js
+++ src/components/Infosection/Data.js
@@ -1,38 +1,39 @@
-import img from "../../images/svg-1.svg"
-import img2 from "../../images/svg-3.svg"
+import img from "../../images/svg-1.svg";
+import img2 from "../../images/svg-3.svg";
 
 export const homeObjOne = {
-    id: 'experience',
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'Web Developer',
-    headline: 'iLearningEngines',
-    description: 'Get access to our exclusive app that allows you to send unlimited transactions without any fees',
-    buttonLabel: 'Get Started',
-    imgStart: false,
-    img: img,
-    alt: 'developer',
-    dark: true,
-    primary: true,
-    darkText: false,
-    button: false
-}
+  id: "experience",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Web Developer",
+  headline: "iLearningEngines",
+  description:
+    "iLearningEngines is an Enterprise Intelligent Platform that drives learning automation and outcomes at scale by leveraging existing enterprise applications and proprietary AI.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: img,
+  alt: "developer",
+  dark: true,
+  primary: true,
+  darkText: false,
+  button: false,
+};
 
 export const homeObjTwo = {
-    id: 'education',
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: true,
-    topLine: 'Govt. Engg. College, Barton Hill, Thiruvananthapuram',
-    headline: 'B.Tech Mechanical Engineering',
-    description: 'CGPA- 8.17',
-    buttonLabel: 'Download Resume',
-    imgStart: true,
-    img: img2,
-    alt: 'graduation',
-    dark: false,
-    primary: false,
-    darkText: true,
-    button: false
-}
+  id: "education",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: true,
+  topLine: "Govt. Engg. College, Barton Hill, Thiruvananthapuram",
+  headline: "B.Tech Mechanical Engineering",
+  description: "CGPA- 8.17",
+  buttonLabel: "Download Resume",
+  imgStart: true,
+  img: img2,
+  alt: "graduation",
+  dark: false,
+  primary: false,
+  darkText: true,
+  button: false,
+};
